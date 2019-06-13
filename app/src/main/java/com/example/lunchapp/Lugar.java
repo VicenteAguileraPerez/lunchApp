71e8bb847f3858be61f07fa7f71f373f05968d9d
V. Alfonso Pérez Espino <taxgype1@gmail.com>
2019-06-12 22:55:08 -0500

    Fragment inicio será el que contenga todos los tipos de comida (pizza, café, etc). Fragment lugares, tendrá el listado del mismo tipo que se haya seleccionado, si selecciona pizza, en ese fragment saldrán todas las pizzerias, si selecciona café, saldrán todos los cafés, etc.
    Fragment lugar, una vez elejido el tipo de comida, y selecciones a un lugar en especifico, este fragment dará información más detallada, de ese lugar en concreto.
    
    El layout toolbar, será la barrá menú (y al tratarse de un layout, podemos scrollearla).
    
    Color nuevo en el resources colors.xml, correspondiente a la barra.
    Nuevo resource dimens.xml, para guardar tamaños.
    
    <b>Hay un error en el navigation.xml @AguileraPerez <b>

--- app/src/main/java/com/example/lunchapp/Lugar.java
+++ app/src/main/java/com/example/lunchapp/Lugar.java
@@ -2,6 +2,7 @@ package com.example.lunchapp;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,19 +12,19 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class selecciondecomidas extends Fragment {
+public class Lugar extends Fragment {
 
 
-    public selecciondecomidas() {
+    public Lugar() {
         // Required empty public constructor
     }
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_selecciondecomidas, container, false);
+        return inflater.inflate(R.layout.fragment_lugar, container, false);
     }
 
 }
--- app/src/main/java/com/example/lunchapp/MainActivity.java
+++ app/src/main/java/com/example/lunchapp/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.lunchapp;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
 
 public class MainActivity extends AppCompatActivity {
     //Commit prueba Victor
     //Commit prueba ZAM1219
+
+    Toolbar toolbar;
+    Fragment fragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.toolbar = findViewById(R.id.tool_bar);
+
+        setSupportActionBar(toolbar);
+        this.fragment = new inicio();
+        
     }
 }
