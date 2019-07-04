c9440146512e60aa2a0ff701ec5674a5c28b9a1c
V. Alfonso Pérez Espino <taxgype1@gmail.com>
2019-07-03 20:54:41 -0500

    Adición de "botón" para abir el menu deslizante

--- app/src/main/java/com/example/lunchapp/MainActivity.java
+++ app/src/main/java/com/example/lunchapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.lunchapp;
 
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -14,6 +17,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //se referencia la toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        //se crea el "botón" para abrir y cerrar el navigation menu
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.abrir_navigation_menu, R.string.cerrar_navigation_menu);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
 
 
