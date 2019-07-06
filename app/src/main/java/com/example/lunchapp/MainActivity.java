d67c8feb470b35d17b8f3739be531405894388e4
vicenteaguilera <vicente_prez@hotmail.com>
2019-07-06 13:59:22 -0500

    correccion del bug al retroceder en el navigation (método activar() y el pressedBack())

--- app/src/main/java/com/example/lunchapp/MainActivity.java
+++ app/src/main/java/com/example/lunchapp/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.lunchapp;
 
-import android.support.design.widget.NavigationView;
-import android.support.v4.app.Fragment;
+
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
