d16b3fba28a30686402648e79a5a2a82bd445b6d
vicenteaguilera <vicente_prez@hotmail.com>
2019-07-07 00:25:48 -0500

    se realizaron pruebas para el funcionamiento del menú pero no se veía bien y se eliminó

--- app/src/main/java/com/example/lunchapp/MainActivity.java
+++ app/src/main/java/com/example/lunchapp/MainActivity.java
@@ -1,16 +1,23 @@
 package com.example.lunchapp;
 
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.navigation.Navigation;
 
 import java.util.Objects;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private boolean activarRetorno;
 
@@ -25,6 +32,10 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         //se crea el "botón" para abrir y cerrar el navigation menu
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.abrir_navigation_menu, R.string.cerrar_navigation_menu);
@@ -52,6 +63,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId())
+        {
+            case R.id.nav_help:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment,new Ayuda()).commit();
+                Toast.makeText(this,"Ayuda",Toast.LENGTH_LONG).show();
+                  break;
+            case R.id.nav_quejas:
+                Toast.makeText(this,"Quejas",Toast.LENGTH_LONG).show();
+                break;
+        }
+        return true;
+    }
+
+    @Override
     public void onBackPressed()
     {
 
@@ -67,4 +93,5 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
+
 }
