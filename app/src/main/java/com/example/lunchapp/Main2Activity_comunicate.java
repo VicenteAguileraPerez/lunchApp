2f6b3a94d0df8c94d4d433a575bc0b1feaca4a57
VicenteAguileraPerez <vicente_prea@hotmail.com>
2019-07-18 15:02:09 -0500

    ajuste en el fragment comunicate

--- app/src/main/java/com/example/lunchapp/Main2Activity_comunicate.java
+++ app/src/main/java/com/example/lunchapp/Main2Activity_comunicate.java
@@ -1,7 +1,11 @@
 package com.example.lunchapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 
 public class Main2Activity_comunicate extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class Main2Activity_comunicate extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2_comunicate);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                View view = this.getCurrentFocus();
+                if (view != null) {
+                    InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+                }
+                break;
+        }
+        return true;
     }
 }
